0ce8d615de331c736239a7ced28be09434425479
Jermaine Braumuller <braumuller02@gmail.com>
2021-03-24 22:59:26 +0100

    Added tests and tried to solve

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,28 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+  def initialize(name, capacity)
+    @name     = name
+    @capacity = capacity
+    @patrons  = []
+    @yell_at_patrons = []
+  end
+
+  def name
+    @name
+  end
+
+  def capacity
+    @capacity
+  end
+
+  def add_patron(patrons)
+    @patrons << patrons ### <= stuck
+  end
+
+  def patrons.upcase |patrons|
+    yell_at_patrons << patrons
+  end
+
 end
